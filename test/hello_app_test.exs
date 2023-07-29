df013a9e85297b1b1a717541468b098fdd3ebbd7
Sean Shubin <sshubin@cj.com>
2023-07-29 15:24:32 -0700

    given when then

--- test/hello_app_test.exs
+++ test/hello_app_test.exs
@@ -2,10 +2,15 @@ defmodule HelloAppTest do
   use ExUnit.Case, async: true
   test "say hello to world" do
     Fake.with_file_io_system fn file, io, system ->
+      # given
       file.set_content("the-file.txt", "world")
       system.set_argv(["the-file.txt"])
       system.set_monotonic_times([1000, 1234])
+
+      # when
       HelloApp.main(file, io, system)
+
+      # then
       actual = io.get_lines()
       expected = ["Hello, world!", "Took 234 microseconds"]
       assert expected == actual
